8f3d9e3d2e75fed830fb39d6bf43ac0dcd265b71
administrator <administrator@administrators-MacBook-Pro.local>
2024-11-19 11:47:03 +0530

    Add missing Serde to MovieAggregatorProcessor. Now we do not need multiple stream config :)

--- src/main/java/com/kstreams/movieratings/MovieAverageRatingsProcessor.java
+++ src/main/java/com/kstreams/movieratings/MovieAverageRatingsProcessor.java
@@ -5,10 +5,7 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -34,7 +31,7 @@ public class MovieAverageRatingsProcessor {
                 .toTable(Materialized.with(Serdes.Long(), ratingSerde));
 
         KTable<Long, RatingAggregator> averageRatings = ratingsTable
-                .groupBy((key, rating) -> KeyValue.pair(rating.movieId(), rating.rating()))
+                .groupBy((key, rating) -> KeyValue.pair(rating.movieId(), rating.rating()), Grouped.with(Serdes.Long(), Serdes.Integer()))
                 .aggregate(
                         () -> new RatingAggregator(0, 0),
                         (Long key, Integer newValue, RatingAggregator aggregate) -> {
--- src/main/java/com/kstreams/wordcount/WordCountProcessor.java
+++ src/main/java/com/kstreams/wordcount/WordCountProcessor.java
@@ -5,7 +5,6 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -16,7 +15,7 @@ public class WordCountProcessor {
     private static final Serde<String> STRING_SERDE = Serdes.String();
 
     @Autowired
-    void buildPipeline(@Qualifier("wordCountStreamsBuilder") StreamsBuilder streamsBuilder) {
+    void buildPipeline(StreamsBuilder streamsBuilder) {
         KStream<String, String> messageStream = streamsBuilder
                 .stream("input-topic", Consumed.with(STRING_SERDE, STRING_SERDE));
 
